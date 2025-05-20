6fce2cf4c700d4086c7788708342e6eb73e5a5a8
Hugo Lesta <hugo.lesta@bestseller.com>
2025-05-20 09:29:42 +0200

    feat: Upgrade to Claude-3.7-sonnet

--- .config/nvim/lua/hugolesta/plugins/avante.lua
+++ .config/nvim/lua/hugolesta/plugins/avante.lua
@@ -7,7 +7,7 @@ return {
     -- add any opts here
     provider = "copilot", -- Recommend using Claude
     copilot = {
-      model = "claude-3.5-sonnet", -- o1-preview | o1-mini | claude-3.5-sonnet
+      model = "claude-3.7-sonnet", -- o1-preview | o1-mini | claude-3.5-sonnet
     },
     auto_suggestions_provider = "copilot",
   },
